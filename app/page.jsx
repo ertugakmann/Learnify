40a7c9d07de36225d486b607f2d8500d84b3209a
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-07-26 10:19:31 +0300

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Feed from "../components/Feed";
+import "../styles/globals.css";
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Welcome to Learnify</h1>
+    </section>
+  );
+};
+
+export default Home;
