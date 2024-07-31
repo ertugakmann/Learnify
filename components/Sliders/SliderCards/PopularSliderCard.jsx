18acce5b620c209d7c6042320313e211f606c86d
Ertuğ Akman <ertug3470@gmail.com>
2024-07-31 21:06:10 +0300

    nextauth loaded

--- components/Sliders/SliderCards/PopularSliderCard.jsx
+++ components/Sliders/SliderCards/PopularSliderCard.jsx
@@ -17,9 +17,9 @@ const PopularSliderCard = () => {
           Dr. Angela Yu, Developer and Lead Instructor
         </p>
 
-        <div class="flex">
+        <div className="flex">
           <svg
-            class="w-4 h-4 text-yellow-300 me-1"
+            className="w-4 h-4 text-yellow-300 me-1"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -28,7 +28,7 @@ const PopularSliderCard = () => {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
           <svg
-            class="w-4 h-4 text-yellow-300 me-1"
+            className="w-4 h-4 text-yellow-300 me-1"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -37,7 +37,7 @@ const PopularSliderCard = () => {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
           <svg
-            class="w-4 h-4 text-yellow-300 me-1"
+            className="w-4 h-4 text-yellow-300 me-1"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -46,7 +46,7 @@ const PopularSliderCard = () => {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
           <svg
-            class="w-4 h-4 text-yellow-300 me-1"
+            className="w-4 h-4 text-yellow-300 me-1"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -55,7 +55,7 @@ const PopularSliderCard = () => {
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
           <svg
-            class="w-4 h-4 text-gray-300 me-1 dark:text-gray-500"
+            className="w-4 h-4 text-gray-300 me-1 dark:text-gray-500"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -63,13 +63,13 @@ const PopularSliderCard = () => {
           >
             <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
           </svg>
-          <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
             4.95
           </p>
-          <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
             out of
           </p>
-          <p class="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
+          <p className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400">
             5
           </p>
         </div>
