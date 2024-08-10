086f2d9e93868b5cb37b0be37884f72bd3a09be6
ertugakmann <134059022+ertugakmann@users.noreply.github.com>
2024-08-10 08:26:50 +0300

    The zustand established to manage statement

---
+++ store/userStore.js
@@ -0,0 +1,12 @@
+// store/userStore.js
+import create from "zustand";
+
+const useUserStore = create((set) => ({
+  userSession: null,
+  isLogged: false,
+  setUserSession: (userSession) =>
+    set({ userSession, isLogged: !!userSession }),
+  logout: () => set({ userSession: null, isLogged: false }),
+}));
+
+export default useUserStore;
