b9c17b25f1a91e0061512fa12a30b20567b8b75f
Ertuğ Akman <ertug3470@gmail.com>
2024-09-08 13:21:54 +0100

    The changes were made on the liked courses section.

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import CourseImage from "@public/assets/course-images/courseimage1.jpg";
 
 const Profile = () => {
     const [userData, setUserData] = useState(null);
@@ -28,10 +29,10 @@ const Profile = () => {
     }, []);
 
     return (
-        <div className="profile-container h-[100vh]">
+        <div className="profile-container mb-4">
             {userData ? (
-                <div className="flex mt-12">
-                    <div className="flex flex-col items-center p-5 h-full shadow-md rounded-md">
+                <div className="flex lg:flex-row flex-col mt-12">
+                    <div className="flex flex-col items-center p-5 h-max-[600px] shadow-md rounded-md">
                         <img
                             src={userData.photoURL}
                             className="rounded-full w-28 h-28 mt-2 object-cover"
@@ -79,65 +80,62 @@ const Profile = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="w-full h-full pl-8 mt-3">
-                        <h2 className="text-4xl font-semibold">
-                            Liked Courses
-                        </h2>
+                    <div className="w-full h-full lg:mt-0 mt-6 lg:pl-8">
+                        <h2 className="text-4xl font-semibold">Liked Courss</h2>
                         <p className="text-md text-gray-500">
                             Check out the courses that {userData.username} liked
                         </p>
-                        <div className="flex gap-4 w-full mt-4">
-                            <div className="w-full flex flex-col gap-y-1">
-                                <Link href={"/"}>
-                                    <img
-                                        src="http://localhost:3000/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Flearnify-2843d.appspot.com%2Fo%2Fcourse_images%252FTGhYvioRpWwEmPzj0Kis%3Falt%3Dmedia%26token%3D756cab30-7272-4257-91ae-0b996bf03fb5&w=640&q=75"
-                                        alt="slider image"
-                                        className="w-[200px] h-[120px]"
-                                    />
-                                    <p className="font-semibold text-md">
-                                        Learn React Native
+                        <div className="flex flex-col gap-4 mt-4">
+                            <div className="w-full flex border-solid border-[2px] gap-3 rounded-lg p-2">
+                                <Image height={200} src={CourseImage} />
+                                <div>
+                                    <p className="text-[17px] font-semibold">
+                                        Advanced C# Crash Course in 24 Hours
                                     </p>
-                                    <p className="text-sm text-gray-400 overflow-hidden whitespace-nowrap text-ellipsis w-64">
-                                        John Doe
-                                    </p>
-
-                                    <div className="flex w-[6rem]">
-                                        <Rating readOnly value={4} />
+                                    <div className="flex flex- w-[6rem] mt-1">
+                                        <Rating readOnly value={4.3} />
                                     </div>
-
-                                    <p className="text-md font-bold">
+                                    <p className="text-md font-bold mt-2">
                                         <span className="text-gray-400 line-through">
                                             $199.99
                                         </span>{" "}
                                         $19.99
                                     </p>
-                                </Link>
+                                </div>
                             </div>
-                            <div className="w-full flex flex-col gap-y-1">
-                                <Link href={"/"}>
-                                    <img
-                                        src="http://localhost:3000/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Flearnify-2843d.appspot.com%2Fo%2Fcourse_images%252FTGhYvioRpWwEmPzj0Kis%3Falt%3Dmedia%26token%3D756cab30-7272-4257-91ae-0b996bf03fb5&w=640&q=75"
-                                        alt="slider image"
-                                        className="w-[200px] h-[120px]"
-                                    />
-                                    <p className="font-semibold text-md">
-                                        Learn React Native
+                            <div className="w-full flex border-solid border-[2px] gap-3 rounded-lg p-2">
+                                <Image height={200} src={CourseImage} />
+                                <div>
+                                    <p className="text-[17px] font-semibold">
+                                        Advanced C# Crash Course in 24 Hours
                                     </p>
-                                    <p className="text-sm text-gray-400 overflow-hidden whitespace-nowrap text-ellipsis w-64">
-                                        John Doe
-                                    </p>
-
-                                    <div className="flex w-[6rem]">
-                                        <Rating readOnly value={4} />
+                                    <div className="flex flex- w-[6rem] mt-1">
+                                        <Rating readOnly value={4.3} />
                                     </div>
-
-                                    <p className="text-md font-bold">
+                                    <p className="text-md font-bold mt-2">
                                         <span className="text-gray-400 line-through">
                                             $199.99
                                         </span>{" "}
                                         $19.99
                                     </p>
-                                </Link>
+                                </div>
+                            </div>
+                            <div className="w-full flex border-solid border-[2px] gap-3 rounded-lg p-2">
+                                <Image height={200} src={CourseImage} />
+                                <div>
+                                    <p className="text-[17px] font-semibold">
+                                        Advanced C# Crash Course in 24 Hours
+                                    </p>
+                                    <div className="flex flex- w-[6rem] mt-1">
+                                        <Rating readOnly value={4.3} />
+                                    </div>
+                                    <p className="text-md font-bold mt-2">
+                                        <span className="text-gray-400 line-through">
+                                            $199.99
+                                        </span>{" "}
+                                        $19.99
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
